e73a0bc9bd064da688b15c23db4293391f8b2fe1
Colin Jupp <colinjupp@gmail.com>
2014-05-18 16:29:15 +0800

    contact form the rails way

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
-LearnRails::Application.routes.draw do
-	post 'contact', to: 'contacts#process_form'
-    root to: 'visitors#new'
+Rails.application.routes.draw do
+resources :contacts, only: [:new, :create]
+root to: 'visitors#new'
 end
-
 
